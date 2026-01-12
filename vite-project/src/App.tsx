906ff2451f1b178aa5b8e1f12a20a53ed5c7ced6
jgnstudios <jim@jgnstudios.com>
2026-01-12 11:53:38 -0800

    create separate header component

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,9 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const HeaderComponent = () => {
   return (
     <>
       <div>
@@ -17,6 +15,16 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+    </>
+  )
+}
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <HeaderComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
